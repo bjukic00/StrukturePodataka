c04de26b7758a35ebdfd7dd9848cedf74af792a4
bjukic00 <73343933+bjukic00@users.noreply.github.com>
2021-04-02 16:28:15 +0200

    Delete borna directory

--- borna/zadatak3.m
+++
@@ -1,9 +0,0 @@
-A = [1 4 6; 3 2 5; 9 2 -6];
-B = [9 23 87; -12 45 4; 2 3 4];
-
-disp(A .* B);
-
-%razlika je u tome što s .* množimo svaki element prve matrice s elementom
-%druge matrice koji se nalazi na istoj poziciji, a običnim množenjem 
-%zbrajamo umnoške svih elemenata n-tog retka i m-tog stupca iste pozicije
-%za rezultantnu matricu gdje će se taj rezultat nalaziti na poziciji (n, m)
